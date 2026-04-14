2a9a7af717338667effe973e78d3a4d28e4289a2
brooke-seeley <brookeseeleyaz@gmail.com>
2026-04-14 16:40:12 -0600

    Finished data collection, began working on modeling and app

---
+++ app.py
@@ -0,0 +1,122 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Load models
+hs_model = joblib.load('hs_model.pkl')
+transfer_model = joblib.load('transfer_jc_model.pkl')
+
+st.title('BYU Commitment Predictor')
+
+# -------------------------------
+# SELECT PLAYER TYPE
+# -------------------------------
+player_type = st.selectbox(
+    'Select Player Type',
+    ['High School', 'Transfer / Junior College']
+)
+
+st.write('Enter player information below:')
+
+yes_no = {
+    'Yes': 'Y',
+    'No': 'N'
+}
+
+conf_map = {
+    'ACC': 'ACC',
+    'Big 12': 'B12',
+    'Big Ten': 'B1G',
+    'SEC': 'SEC',
+    'Group of 6/Independent': 'G6I',
+    'FCS School': 'FCS',
+    'Junior College': 'JC'
+}
+
+# ===============================
+# HIGH SCHOOL FORM
+# ===============================
+
+if player_type == 'High School':
+
+    top247 = st.selectbox('Top 247 Recruit?', yes_no.keys())
+    position = st.selectbox('Position', ['CB', 'DB', 'DL', 'LB', 'OL', 'QB', 'RB', 'TE', 'WR'])
+    utah = st.selectbox('From Utah?', yes_no.keys())
+    distance = st.number_input('Distance from BYU (miles)', min_value=0.0)
+    height = st.number_input('Height (inches)', min_value=60.0)
+    weight = st.number_input('Weight (lbs)', min_value=150.0)
+    score = st.number_input('247 Composite Score', min_value=0.0, max_value=1.0)
+
+    lds = st.selectbox('LDS?', yes_no.keys())
+    alumni = st.selectbox('Alumni Connection?', yes_no.keys())
+    poly = st.selectbox('Polynesian?', yes_no.keys())
+
+    input_data = pd.DataFrame({
+        '247Top': [top247],
+        'Position': [position],
+        'Utah': [utah],
+        'Distance': [distance],
+        'Height': [height],
+        'Weight': [weight],
+        'Score': [score],
+        'LDS': [lds],
+        'Alumni': [alumni],
+        'Poly': [poly]
+    })
+
+    model = hs_model
+
+# ===============================
+# TRANSFER / JC FORM
+# ===============================
+else:
+
+    years = st.number_input('Years of Eligibility Remaining', min_value=0)
+    top247 = st.selectbox('Top 247 Transfer', yes_no.keys())
+    position = st.selectbox('Position', ['CB', 'DL', 'LB', 'OL', 'QB', 'RB', 'TE', 'WR'])
+    distance = st.number_input('Distance from BYU (miles)', min_value=0.0)
+
+    conf = st.selectbox('Conference', conf_map.keys())
+
+    height = st.number_input('Height (inches)', min_value=60.0)
+    weight = st.number_input('Weight (lbs)', min_value=150.0)
+    score = st.number_input('247 Composite Score', min_value=0.0, max_value=1.0)
+
+    lds = st.selectbox('LDS?', yes_no.keys())
+    alumni = st.selectbox('Alumni Connection?', yes_no.keys())
+    prev = st.selectbox('Offered in High School?', yes_no.keys())
+    poly = st.selectbox('Polynesian?', yes_no.keys())
+
+    input_data = pd.DataFrame({
+        'Years': [years],
+        '247Top': [top247],
+        'Position': [position],
+        'Distance': [distance],
+        'Conf': [conf],
+        'Height': [height],
+        'Weight': [weight],
+        'Score': [score],
+        'LDS': [lds],
+        'Alumni': [alumni],
+        'Prev': [prev],
+        'Poly': [poly]
+    })
+
+    model = transfer_model
+
+# -------------------------------
+# PREDICTION BUTTON
+# -------------------------------
+if st.button("Predict Commitment Probability"):
+
+    prob = model.predict_proba(input_data)[0][1]
+
+    st.subheader(f"Probability of Commitment: {prob:.3f}")
+    st.progress(float(prob))
+
+    if prob > 0.7:
+        st.success("High likelihood of committing to BYU")
+    elif prob > 0.4:
+        st.warning("Moderate likelihood")
+    else:
+        st.error("Low likelihood")
